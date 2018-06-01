5bc3dd7b92d35044a1e1fc237bc1cda6b4f435ee
Anna Goldstein <apgoldst@gmail.com>
2018-06-01 15:17:57 -0400

    new sandbox

--- sandbox.py
+++ sandbox.py
@@ -3,104 +3,37 @@ import submit_search
 import wok_soap
 import xml.etree.ElementTree as ET
 import csv
+import os
+import time
 
 
-csv_file = "DOE grant short list.csv"
 
-# print len(ns)
+
+#SID = wok_soap.auth()
+#print(SID)
+
+csv_file = "example DOIs.csv"
+SID="8BIaALTznDdvcPRBWqF"
+
+save_tables.print_pub_table_from_DOIs(csv_file)
+
+
+
+
 #
-# SID = wok_soap.auth()
-# print SID
-
-SID = "2WK3NwAyUj4xxl6RWiM"
-UID = "WOS:000283490400005"
-
-submit_search.search_for_citing_articles(UID, SID)
-
-
-# data = []
-# data.append({"__paper list": []})
-# data.append({"__paper list": [1,2,3,4]})
-# print data
-# print data[1]["__paper list"][0]
+#filename = "DOI search results xml/DO = 10.1002 9781118877517.ch7.txt"
 #
-# for grant in data:
-#     example_paper = grant["__paper list"]
-#     print example_paper
-#     if example_paper:
-#         break
+## open search results file and parse as XML
+#with open(filename) as h:
+#    tree = ET.parse(h)
+#    print(str(tree))
+#    root = tree.getroot()
+#    
+#if root:
+#    print(root[0])
+#    
+#    
 #
-# print "example_paper is " + str(example_paper)
-
-
-
-# SID = wok_soap.auth()
-# print SID
-#
-# UID = "WOS:000346178800058"
-# results = wok_soap.citedReferences(UID, SID)
-# print results
-
-# # Test XML parsing for first record in a file of search results
-# filename = "grant search results - full html/FT = AR0000007 OR FT = AR 0000007.txt"
-# with open(filename, "r+") as f:
-#     tree = ET.parse(f)
-# root = tree.getroot()
-# record = root[0]
-# static_data = record[1]
-# summary = static_data[0]
-# fullrecord_metadata = static_data[1]
-# item = static_data[2]
-# dynamic_data = record[2]
-
-# query = "FT = SC0000957 OR FT = SC 0000957"
-# SID = wok_soap.auth()
-# print SID
-# SID = "1WEwqdk8PcphC1aOTiI"
-# #
-# search_result = wok_soap.search(query,SID)
-# queryId = search_result[0]
-# records_found = search_result[1]
-# results_list = search_result[3]
-# print len(results_list)
-#
-# print str(records_found) + " records found"
-# with open("search result.txt", "wb") as f:
-#     f.write(str(search_result))
-
-# retrieve_result = wok_soap.retrieve(queryId,SID,start_count=101)
-# with open("retrieve result.txt", "wb") as f:
-#     f.write(str(retrieve_result))
-# #
-# # paper with 157 references
-# UID = "WOS:000275858200003"
-
-# cited_refs_result = wok_soap.citedReferences(UID,SID)
-# queryId = cited_refs_result[0]
-# with open("cited refs result.txt", "wb") as f:
-#     f.write(str(cited_refs_result))
-#
-# cited_refs_retrieve_result = wok_soap.citedReferencesRetrieve(queryId,SID,start_count=101)
-# with open("cited refs retrieve result.txt", "wb") as f:
-#     f.write(str(cited_refs_retrieve_result))
-# print type(cited_refs_retrieve_result)
-# print dir(cited_refs_retrieve_result)
-# print type(cited_refs_retrieve_result[0])
-# print dir(cited_refs_retrieve_result[0])
-# print cited_refs_retrieve_result[0].year
-
-# print "results[1][0] = " + str(results[1][0]) # first cited ref
-# print "results[1][0][0] = " + str(results[1][0][0]) # docid
-# print dir(results[1][0])
-# print results[1][0].citedWork
-
-# UID = "000341217500093"
-# results = wok_soap.retrieveById(UID, SID)
-# print results[3]
-# tree = ET.fromstring(results[3])
-# refs = tree.find(".//" + ns + "refs")
-# ref_count = refs.attrib['count']
-# submit_search.search_for_cited_refs(UID, SID, ref_count)
-
-# submit_search.search_for_cited_refs(UID,SID)
-# submit_search.search_by_grant("FT = AR0000007 OR FT = AR 0000007.csv", SID)
+#record = root[0]
+#paper = save_tables.process_article(record)
+#paper = save_tables.citation_analysis(paper, SID, counter)
